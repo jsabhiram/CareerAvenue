41b16c390a4ed79db7cf9dd458de0813ece82743
J S Abhiram Suresh <jsabhiramsuresh@gmail.com>
2025-07-19 10:02:13 +0530

    technical mock

---
+++ q1.java
@@ -0,0 +1,35 @@
+import java.util.*;
+ class q1 
+{
+    public static void main(String []args)
+    {
+    Scanner sc = new Scanner (System.in);
+    System.out.println("Enter the number of monkeys");
+    int n = sc.nextInt(); // number of monkeys
+
+    System.out.println("Enter the number of monkeys");
+    int k = sc.nextInt(); //total number of bananas
+
+    System.out.println("Enter the number of monkeys");
+    int j = sc.nextInt(); // ttotal number of peanuts
+
+    System.out.println("Enter the number of monkeys");
+    int m = sc.nextInt(); // capacity of bananas
+
+    System.out.println("Enter the number of monkeys");
+    int p = sc.nextInt(); // capacity of peanuts
+
+    
+    sc.close();
+}
+}
+// Day 2 Slot 2 Question 2
+// Click on the below button to study Day 2 Slot 2 Question 1 of TCS NQT Coding 2020 exam
+// 
+// Code
+// 
+// TCS Coding Questions
+// Click on the below button to study more TCS Coding Question
+// 
+// TCS Coding Question
+
