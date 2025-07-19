7a4fcc0929e1324fd9543479c1a13603fb6a7eee
J S Abhiram Suresh <jsabhiramsuresh@gmail.com>
2025-07-19 10:41:04 +0530

    change

--- q2_ch.java
+++ q2_ch.java
@@ -1,24 +1,14 @@
 public class q2_ch {
     public static void main(String[] args) {
-        q2_ch obj = new q2_ch();
-        System.out.println("10th Fibonacci number: " + obj.fib(10));
-        System.out.println("5th Prime number: " + obj.cal_prime(5));
+        q2_ch obj = new q2_ch();//System.out.println("10th Fibonacci number: " + obj.fib(10));System.out.println("5th Prime number: " + obj.cal_prime(5));
+        obj.core(14);
+        // obj.core(5);
     }
 
     // Function to return the nth Fibonacci number
     public int fib(int n) {
-        if (n <= 0) return 0;
-        if (n == 1) return 0;
-        if (n == 2) return 1;
-
-        int e1 = 0, e2 = 1, result = 0;
-        for (int i = 3; i <= n; i++) {
-            result = e1 + e2;
-            e1 = e2;
-            e2 = result;
-        }
-        return result;
-    }
+        if (n <= 0) return 0;if (n == 1) return 1;if (n == 2) return 1;int e1 = 0, e2 = 1, result = 0;
+        for (int i = 3; i <= n; i++) {result = e1 + e2;e1 = e2;e2 = result;} return result;}
 
     // Function to return the nth prime number
     public int cal_prime(int n) {
@@ -26,15 +16,11 @@ public class q2_ch {
         int num = 2;
 
         while (true) {
-            if (isPrime(num)) {
-                count++;
-                if (count == n) {
-                    return num;
-                }
-            }
+            if (isPrime(num)) {count++;if (count == n) { return num;}}
             num++;
         }
     }
+    public void core(int n) {if(n==0){System.out.println("Invalid input");}if(n%2==0){System.out.println(fib(n));}else{System.out.println(cal_prime(n));}}
 
     // Helper function to check if a number is prime
     private boolean isPrime(int num) {
