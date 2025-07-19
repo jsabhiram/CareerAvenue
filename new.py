865cfd79a4394ae4016dee4ece1fabb7fff243ba
J S Abhiram Suresh <jsabhiramsuresh@gmail.com>
2025-07-19 13:40:31 +0530

    partially done python chain

--- new.py
+++ new.py
@@ -4,18 +4,35 @@ class Monster:
         self.bonus=y
     def desc(self):
         return {self.power+self.bonus}
-    
+def srt(lst):
+    for i in range(len(lst)):
+        for j in range(i+1,len(lst)):
+            if(lst[i].power>lst[j].power):
+                temp=lst[i]
+                lst[i]=lst[j]
+                lst[j]=temp
+    return lst
 
+data=[]
 print("--------------------------------------------------")
 n=(input("Enter the game stats:"))
 #process
 try:
     total=n.split(" ")
-    n=total[0],exp=total[1]
-    for i in range(2,len(total)-2):
-        m=Monster(total[i],total[i+1])
-        m.desc()
+    print(total)
+    n=int(total[0])
+    exp=int(total[1])
+    # print("Error")
+    c=2
+    while(c<len(total)):
+        m=Monster(total[c],total[c+1])
+        c=c+2
+        print("Entered")
+        data.append(m)
+        new_data=srt(data)
+        
 except:
     print("Invalid input")
-
+for i in new_data:
+    print(i.desc())
 print("--------------------------------------------------")
